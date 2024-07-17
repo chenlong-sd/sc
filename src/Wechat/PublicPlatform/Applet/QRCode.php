0f78dd57e924a7f6ed1c966a42ce12a3690407db
chenlong <vip_chenlong@163.com>
2024-07-17 18:04:01 +0800

    .

--- src/Wechat/PublicPlatform/Applet/QRCode.php
+++ src/Wechat/PublicPlatform/Applet/QRCode.php
@@ -28,16 +28,16 @@ class QRCode
     }
 
     /**
-     * access_token    string    是    接口调用凭证，该参数为 URL 参数，非 Body 参数。使用getAccessToken 或者 authorizer_access_token
-     * scene    string    是    最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
-     * page    string    否    默认是主页，页面 page，例如 pages/index/index，根路径前不要填加 /，不能携带参数（参数请放在scene字段里），如果不填写这个字段，默认跳主页面。
-     * check_path    bool    否    默认是true，检查page 是否存在，为 true 时 page 必须是已经发布的小程序存在的页面（否则报错）；为 false 时允许小程序未发布或者 page 不存在， 但page 有数量上限（60000个）请勿滥用。
-     * env_version    string    否    要打开的小程序版本。正式版为 "release"，体验版为 "trial"，开发版为 "develop"。默认是正式版。
-     * width    number    否    默认430，二维码的宽度，单位 px，最小 280px，最大 1280px
-     * auto_color    bool    否    自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调，默认 false
-     * line_color    object    否    默认是{"r":0,"g":0,"b":0} 。auto_color 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"} 十进制表示
+     * <li>access_token    string    是    接口调用凭证，该参数为 URL 参数，非 Body 参数。使用getAccessToken 或者 authorizer_access_token</li>
+     * <li>scene    string    是    最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）</li>
+     * <li>page    string    否    默认是主页，页面 page，例如 pages/index/index，根路径前不要填加 /，不能携带参数（参数请放在scene字段里），如果不填写这个字段，默认跳主页面。</li>
+     * <li>check_path    bool    否    默认是true，检查page 是否存在，为 true 时 page 必须是已经发布的小程序存在的页面（否则报错）；为 false 时允许小程序未发布或者 page 不存在， 但page 有数量上限（60000个）请勿滥用。</li>
+     * <li>env_version    string    否    要打开的小程序版本。正式版为 "release"，体验版为 "trial"，开发版为 "develop"。默认是正式版。</li>
+     * <li>width    number    否    默认430，二维码的宽度，单位 px，最小 280px，最大 1280px</li>
+     * <li>auto_color    bool    否    自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调，默认 false</li>
+     * <li>line_color    object    否    默认是{"r":0,"g":0,"b":0} 。auto_color 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"} 十进制表示</li>
      *      属性    类型    必填    说明
-     *      is_hyaline    bool    否    默认是false，是否需要透明底色，为 true 时，生成透明底色的小程序
+     *      <li>is_hyaline    bool    否    默认是false，是否需要透明底色，为 true 时，生成透明底色的小程序</li>
      * 返回参数
      * 数量无限制
      * @param array|string $param
