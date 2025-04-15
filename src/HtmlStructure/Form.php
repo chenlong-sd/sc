6b9a58fc39aefdce738956c20f40cd742eb8483e
陈龙 <vip_chenlong@163.com>
2025-04-15 18:17:20 +0800

    1

--- src/HtmlStructure/Form.php
+++ src/HtmlStructure/Form.php
@@ -19,7 +19,7 @@ use Sc\Util\HtmlStructure\Html\Js\Grammar;
 use Sc\Util\HtmlStructure\Theme\Interfaces\FormThemeInterface;
 use Sc\Util\HtmlStructure\Theme\Theme;
 
-class Form extends \Sc\Util\HtmlElement\ElementType\AbstractHtmlElement
+class Form
 {
     /**
      * @var array|mixed
